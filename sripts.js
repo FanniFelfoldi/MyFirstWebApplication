76c9526867ce666b650a622301bd4a99e388bbe6
FanniFelfoldi <62945610+FanniFelfoldi@users.noreply.github.com>
2020-04-22 19:07:39 +0200

    mod.22.04

--- sripts.js
+++ sripts.js
@@ -1,60 +1,60 @@
-let data = {
+var data1 = {
     photo: "/Pictures/biscottini.jpg",
     title: "Biscottini", 
     description: "Biscottini per la nascita. Biscottini con pastafrolla.",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data2 = {
     photo: "/Pictures/bonbons.jpg",
     title: "Bon-Bons", 
     description: "Bon-bons repieni con la crema pistachio o marmellata di fichi",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data3 = {
     photo: "/Pictures/bread.jpg",
     title: "Pane", 
     description: "Pane fatto in casa con lievito madre",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data4 = {
     photo: "/Pictures/fruitcake.jpg",
     title: "Torta Margherita alla frutta", 
     description: "Torta Margherita alla frutta mista, per un compleanno",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data5 = {
     photo: "/Pictures/howToTrainYourDragon.jpg",
     title: "How to train your dragon", 
     description: "Torta al ferrero roche, con un Dragone di marzipano",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data6 = {
     photo: "/Pictures/kuglof.jpg",
     title: "Ciambella", 
     description: "Ciambella, kuglof con lievito madre",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data7 = {
     photo: "/Pictures/pizza.jpg",
     title: "Pizza fatta in casa", 
     description: "Pizza step by step.",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data8 = {
     photo: "/Pictures/treccina.jpg",
     title: "Treccina", 
     description: "Treccina con zafferano.",
 };
-$("photo").attr("src", data.photo);
+$("#photo").attr("src", data.photo);
 
-let data = {
+var data9 = {
     photo: "/Pictures/trotaMargherita.jpg",
     title: "Torta Margherita", 
     description: "Torta Margherita alla cacao",
@@ -64,3 +64,13 @@ $("#photo").attr("src", data.photo);
 let imagesData = ["/Pictures/biscottini.jpg", "/Pictures/bonbons.jpg", "/Pictures/bread.jpg", "/Pictures/fruitcake.jpg", "/Pictures/howToTrainYourDragon.jpg", "/Pictures/kuglof.jpg", "/Pictures/pizza.jpg", "/Pictures/treccina.jpg", "/Pictures/trotaMargherita.jpg"];
 let currentPhoto = 0;
 $("#photo").attr("src", imagesData[currentPhoto].photo);
+
+let loadPhoto = (photoNumber) => {
+    $("#photo").attr("src", imagesData[photoNumber].photo);
+}
+loadPhoto (currentPhoto);
+
+$("#right").click (() => {
+    currentPhoto++;
+    loadPhoto(currentPhoto);
+})
