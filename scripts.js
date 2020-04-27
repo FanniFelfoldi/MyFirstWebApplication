b6825dfb682611e8a54e53283834c6c165fde7ac
FanniFelfoldi <62945610+FanniFelfoldi@users.noreply.github.com>
2020-04-27 17:50:04 +0200

    title, description done,

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let name = 'Fanni';
+console.log(`Hello, Fanni!`)
+
+//top part
 
 var data1 = {
     photo: "/Pictures/biscottini.jpg",
@@ -52,18 +56,16 @@ var data9 = {
     description: "Torta Margherita alla cacao",
 };
 
-
-
-let imagesData = [data1, data2, data3, data4, data5, data6, data7, data8, data9,];
-let currentPhoto = 0;
-
+var currentPhoto = 0;
+var imagesData = [data1, data2, data3, data4, data5, data6]
 $('#photo').attr('src', imagesData[currentPhoto].photo);
 $('#photo-title').text(imagesData[currentPhoto].title);
 $('#photo-description').text(imagesData[currentPhoto].description);
 
+function loadPhoto(photoNumber) {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
 
-function loadTitle(photoNumber) {
-$("#photo").attr("src", imagesData[currentPhoto]);
+
 }
 
 function loadTitle(photoNumber) {
@@ -75,54 +77,3 @@ function loadDescription(photoNumber) {
 }
 
 loadPhoto(currentPhoto);
-
-
-
-
-
-$("#right").click (() => {
-    currentPhoto++;
-    loadPhoto(currentPhoto);
-    loadTitle(currentPhoto);
-    loadDescription(currentPhoto);
-})
-
-$("#left").click (() => {
-    currentPhoto--;
-    loadPhoto(currentPhoto);
-    loadTitle(currentPhoto);
-    loadDescription(currentPhoto);
-})
-
-//ezt nezd meg https://www.youtube.com/watch?v=7ZO2RTMNSAY
-// hogy a legutolso kepre ugorjo - le kell meg ezt tisztazni
-
-left.addEventListener ("click", function (){
-    if(currentPhoto===0) {
-        currentPhoto = viewer.length;
-    }
-    slideLeft();
-})
-
-// hogy a legelso kepre ugorjo, le kell meg tisztazni a kodokat
-arrowRight.addEventListener ("click", function (){
-    if(current===sliderImages.length - 1) {
-        current = -1;
-    }
-    slideRight();
-})
-
- // vissza lép  
- $('#images-box').on('click', '.previous', (event) => {
-    plusSlides(-1);
-  });
-  // előre lép
-  $('#images-box').on('click', '.next', (event) => {
-    plusSlides(1);
-  });
-
-  $(document).ready(function() {
-    $('.photoScroll img').click(function(e) {
-        $('.photoHolder img').attr("src", $(this).attr("src"));
-    })
-}); 
