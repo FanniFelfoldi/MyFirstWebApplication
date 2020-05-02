30abf555dcf1afb46bdaf80cf531eb94baf83520
FanniFelfoldi <62945610+FanniFelfoldi@users.noreply.github.com>
2020-05-02 15:54:55 +0200

    makeOrderInTheFolderOfGallery

--- scriptsGallery.js
+++ scriptsGallery.js
@@ -4,54 +4,54 @@ console.log(`Hello, Fanni!`)
 //top part
 
 var data1 = {
-    photo: "/Pictures/biscottini.jpg",
+    photo: "/images/biscottini.jpg",
     title: "Biscottini", 
     description: "Biscottini per la nascita. Biscottini con pastafrolla.",
 };
 var data2 = {
-    photo: "/Pictures/bonbons.jpg",
+    photo: "/images/bonbons.jpg",
     title: "Bon-Bons", 
     description: "Bon-bons repieni con la crema pistachio o marmellata di fichi",
 };
 
 var data3 = {
-    photo: "/Pictures/bread.jpg",
+    photo: "/images/bread.jpg",
     title: "Pane", 
     description: "Pane fatto in casa con lievito madre",
 };
 
 var data4 = {
-    photo: "/Pictures/fruitcake.jpg",
+    photo: "/images/fruitcake.jpg",
     title: "Torta Margherita alla frutta", 
     description: "Torta Margherita alla frutta mista, per un compleanno",
 };
 
 var data5 = {
-    photo: "/Pictures/howToTrainYourDragon.jpg",
+    photo: "/images/howToTrainYourDragon.jpg",
     title: "How to train your dragon", 
     description: "Torta al ferrero roche, con un Dragone di marzipano",
 };
 
 var data6 = {
-    photo: "/Pictures/kuglof.jpg",
+    photo: "/images/kuglof.jpg",
     title: "Ciambella", 
     description: "Ciambella, kuglof con lievito madre",
 };
 
 var data7 = {
-    photo: "/Pictures/pizza.jpg",
+    photo: "/images/pizza.jpg",
     title: "Pizza fatta in casa", 
     description: "Pizza step by step.",
 };
 
 var data8 = {
-    photo: "/Pictures/treccina.jpg",
+    photo: "/images/treccina.jpg",
     title: "Treccina", 
     description: "Treccina con zafferano.",
 };
 
 var data9 = {
-    photo: "/Pictures/trotaMargherita.jpg",
+    photo: "/images/trotaMargherita.jpg",
     title: "Torta Margherita", 
     description: "Torta Margherita alla cacao",
 };
